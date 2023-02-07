bdb471f38d8c9d185949126113fc361d325ed4a0
HP <HP@DESKTOP-5FKCQM5>
2023-02-07 14:24:52 +0530

    puntter

---
+++ RCCE23/src/sagar/Punter1.java
@@ -0,0 +1,14 @@
+package sagar;
+
+public class Punter1 
+{
+	public int hashcode()
+	{
+		return 143;
+		
+	}
+public static void main(String[] args) {
+	Punter1 p =new Punter1();
+	System.out.println(p.hashcode());
+}
+}
